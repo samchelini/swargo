e2d04de5e9ca4f41d2df3ec944203d74f6015b59
Samuel Chelini <samchelini@gmail.com>
2025-02-28 20:30:30 -0500

    added brightness block

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"github.com/samchelini/swargo/bar"
+  "log"
+  "os"
 )
 
 func main() {
+  log.SetOutput(os.Stderr) // log to stderr
+
+  // initialize bar
 	b := bar.NewBar()
+
+  // create and add a DateTimeBlock
 	dateTimeBlock := new(bar.DateTimeBlock)
 	b.AddBlock(dateTimeBlock)
+
+  // create and add a BrightnessBlock
+  brightnessBlock := new(bar.BrightnessBlock)
+  brightnessBlock.SetDir("/sys/class/backlight/intel_backlight")
+  b.AddBlock(brightnessBlock)
+
+  // run the bar
 	b.Run()
 }
